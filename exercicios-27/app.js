5e7fc81c0be0886c50dca52821861ead27251d0b
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2022-11-21 07:27:04 -0300

    refatorando ultimo exercicio

--- exercicios-27/app.js
+++ exercicios-27/app.js
@@ -121,25 +121,22 @@ console.log(myFunc('Rafael', 'Alves', 35))
 let booksBox = {
   spaces: 5,
   booksIn: 0
-}(
-  // const getPluralOrSingular = (quantity, singular, plural) =>
+}
+booksBox.addBooks = booksQuantity => {
+  if (booksBox.booksIn === booksBox.spaces) {
+    return `A caixa já está cheia`
+  }
 
-  (booksBox.addBooks = booksQuantity => {
-    if (booksBox.booksIn === booksBox.spaces) {
-      return `A caixa já está cheia`
-    }
+  const availableSpaces = booksBox.spaces - booksBox.booksIn
+  const bookPluralorSingular = availableSpaces === 1 ? 'livro' : 'livros'
 
-    const availableSpaces = booksBox.spaces - booksBox.booksIn
-    const bookPluralorSingular = availableSpaces === 1 ? 'livro' : 'livros'
-
-    if (booksBox.booksIn + booksQuantity > booksBox.spaces) {
-      const fitPluralOrSingular = availableSpaces === 1 ? 'cabe' : 'cabem'
-      return `Só ${fitPluralOrSingular} mais ${availableSpaces} ${bookPluralorSingular} na caixa`
-    }
-    booksBox.booksIn += booksQuantity
-    return `Já há ${booksBox.booksIn} ${bookPluralorSingular} na caixa`
-  })
-)
+  if (booksBox.booksIn + booksQuantity > booksBox.spaces) {
+    const fitPluralOrSingular = availableSpaces === 1 ? 'cabe' : 'cabem'
+    return `Só ${fitPluralOrSingular} mais ${availableSpaces} ${bookPluralorSingular} na caixa`
+  }
+  booksBox.booksIn += booksQuantity
+  return `Já há ${booksBox.booksIn} ${bookPluralorSingular} na caixa`
+}
 
 console.log(booksBox.addBooks(2))
 console.log(booksBox.addBooks(3))
